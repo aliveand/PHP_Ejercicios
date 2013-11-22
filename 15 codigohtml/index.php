d4b3c7966621c24ff98b12446a6000ead2272126
aliveand <g9-angelito@hotmail.com>
2013-11-22 12:00:29 +0100

    listo

---
+++ 15 codigohtml/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<tittle></tittle>
+</head>
+<body>
+<?php
+echo '
+<form action="html.php" method="GET">
+Introduce codigo HTML<br>
+<textarea rows="4" cols="50" name="string">
+</textarea><br>
+<input type="submit" />
+</form>';
+?>
+</body>
+</html>
